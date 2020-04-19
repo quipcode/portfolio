d16b0da0012f5921d4bcc20ee0c1b3bb5935997d
quipcode <abdulkarim8ahmed@gmail.com>
2020-04-19 01:56:42 -0700

    added experience and contact

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from './header/Header'
 import Skills from './skills/Skills'
 import Projects from './projects/Projects'
+import Experience from './experience/Experience'
+import Contact from './contact/Contact'
 
 export default class Main extends Component {
     render(){
@@ -11,6 +13,8 @@ export default class Main extends Component {
                 <Header/>
                 <Skills/>
                 <Projects/>
+                <Experience/>
+                <Contact/>
             </div>
         )
     }
